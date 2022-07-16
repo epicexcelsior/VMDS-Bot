b15d90153d4c145ddc266368dfb082a9de45222c
epicexcelsior <legoawesome@icloud.com>
2022-07-16 13:33:32 -0800

    Update archive-threads.js

--- commands/archive-threads.js
+++ commands/archive-threads.js
@@ -5,20 +5,22 @@ module.exports = {
 		.setName('archive-threads')
 		.setDescription('Archives all threads'),
 	async execute(interaction) {
-        try {
-            const activeThreads = await interaction.guild.channels.fetchActiveThreads();
-            await interaction.deferReply({ ephemeral: true })
+        if (interaction.user.id === '295227446981033984') {
+            try {
+                const activeThreads = await interaction.guild.channels.fetchActiveThreads();
+                await interaction.deferReply({ ephemeral: true })
 
-            for (const elem of activeThreads.threads.values()) {
-                await elem.edit( {archived: true} )
-                    .then(console.log(`${interaction.user.tag} archived thread: ${elem.name}`))
-                    .catch((error) => console.error(error))
-            };
-            
-            await interaction.editReply('<a:aRight:978722165832695849> Successfully archived threads.');
-        } catch (error) {
-            await interaction.editReply({content: '<a:aWrong:978722165933359174> An error occurred.', ephemeral: true});
-            console.error(error);
+                for (const elem of activeThreads.threads.values()) {
+                    await elem.edit( {archived: true} )
+                        .then(console.log(`${interaction.user.tag} archived thread: ${elem.name}`))
+                        .catch((error) => console.error(error))
+                };
+                
+                await interaction.editReply('<a:aRight:978722165832695849> Successfully archived threads.');
+            } catch (error) {
+                await interaction.editReply({content: '<a:aWrong:978722165933359174> An error occurred.', ephemeral: true});
+                console.error(error);
+            }
         }
     }
 };
