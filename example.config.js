eda1b1af5d74423c8b579acd8e334e1a9194d9b7
Excelsior <33706074+epicexcelsior@users.noreply.github.com>
2022-08-03 22:08:42 -0800

    Update example.config.js

--- example.config.js
+++ example.config.js
@@ -12,107 +12,32 @@ module.exports = {
         emoji: '⚙'
     },
 
+    // button.customId MUST match the category name
     roleData: {
-        vexentoNotifs: {
+        roleCategoryName: {
             roles: {
-                youtubeNotifs: {
-                    label: 'YouTube Notifications',
-                    value: '906980734529572866',
-                    description: 'Description 1',
-                    emoji: '1️⃣'
+                role1: {
+                    label: 'Label',
+                    value: 'ROLE ID',
+                    description: 'Description',
+                    emoji: '⭐'
                 },
-                twitterNotifs: {
-                    label: 'Twitter Notifications',
-                    value: '906980761079513119',
-                    description: 'Description 1',
-                    emoji: '1️⃣'
+                role2: {
+                    label: 'Label',
+                    value: 'ROLE ID',
+                    description: 'Description',
+                    emoji: '👍'
                 },
             },
             button: {
-                customId: 'vexentoNotifs',
-                label: 'Vexento Notifications',
+                customId: 'roleCategoryName',
+                label: 'Label',
                 style: 1,
                 emoji: '🔔'
             },
             menu: {
-                placeholder: 'notifs placeholder!!!',
-                message: '> Wow test message goes here!',
-            },
-        },
-        serverNotifs: {
-            roles: {
-                serverAnnouncements: {
-                    label: 'Server Announcement Notifications',
-                    value: '906980783481323581',
-                    description: 'Description 1',
-                    emoji: '1️⃣',
-                },
-                eventNotifs: {
-                    label: 'Event Notifications',
-                    value: '1003478799746347079',
-                    description: 'Description 1',
-                    emoji: '1️⃣',
-                },
-            },
-            button: {
-                customId: 'serverNotifs',
-                label: 'Server Notifications',
-                style: 1,
-                emoji: '📣'
-            },
-            menu: {
-                placeholder: 'Server notifs placeholder',
-                message: '> Wow test message goes here!',
-            },
-        },  
-        activities: {
-            roles: {
-                producer: {
-                    label: 'Producer',
-                    value: '906981440359657533',
-                    description: 'vexento ayayay',
-                    emoji: '🎹'
-                },
-                musician: {
-                    label: 'Musician',
-                    value: '1002413794829553674',
-                    description: 'ladida',
-                    emoji: '🎷'
-                },
-                singer: {
-                    label: 'Singer/Vocalist',
-                    value: '1002413841210155068',
-                    description: 'Sing!!',
-                    emoji: '🎙'
-                },
-                designer: {
-                    label: 'Designer',
-                    value: '1002414423417294899',
-                    description: 'Design stuffs',
-                    emoji: '🎨'
-                },
-                contentCreator: {
-                    label: 'Content Creator',
-                    value: '906981526829400105',
-                    description: 'CCCCC',
-                    emoji: '🎥'
-                },
-                gamer: {
-                    label: 'Gamer',
-                    value: '1002413938610282599',
-                    description: 'Gameing',
-                    emoji: '🎮'
-                },
-            },
-            button: {
-                customId: 'activities',
-                label: 'Activities',
-                style: 1,
-                emoji: '🎈'
-            },
-            menu: {
-                placeholder: 'Activites placeholder',
-                message: '> Wow test message goes here!',
+                placeholder: 'Select Menu Placeholder',
+                message: '> Select your roles below!',
             },
         },  
     },
