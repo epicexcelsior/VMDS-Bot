b48cf497fa8bed0dac7d7ceaf1aa1ad7bfdbfcb4
epicexcelsior <epicexcelsior@gmail.com>
2022-11-09 09:28:58 -0900

    Create fixTimes.js

---
+++ commands/fixTimes.js
@@ -0,0 +1,16 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('fix-times')
+        .addIntegerOption(option =>
+            option.setName('end_unix')
+                .setDescription('end time of movie in Unix seconds')
+                .setRequired(true))
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageEvents),
+	async execute(client, interaction) {        
+        const endTime = interaction.options.getInteger('end_unix');
+        client.unix = endTime;
+        await interaction.reply({ content: `Movie form end time set to <t:${client.unix}:F>`, ephemeral: true })
+    }
+}
