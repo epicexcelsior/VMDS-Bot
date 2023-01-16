b84c663375fb00172d6049af49ef9220b0dcde86
epicexcelsior <legoawesome@icloud.com>
2023-01-16 13:06:49 -0900

    Update roleButtons.js

--- functions/roleButtons.js
+++ functions/roleButtons.js
@@ -8,15 +8,9 @@ for (let i in roleData) {
 };
 
 async function makeButtons (interaction) {
-<<<<<<< HEAD
-    try {
-        if (!(validButtonIds.includes(interaction.customId))) return;
-
-=======
     if (!(validButtonIds.includes(interaction.customId))) return;
     
     try {
->>>>>>> dev
         // Parses roleData to array to use in select menu creation
         selectMenuOptions = [];
         const categoryRoles = roleData[interaction.customId].roles;
@@ -28,37 +22,6 @@ async function makeButtons (interaction) {
             };
             selectMenuOptions.push(categoryRoles[i]);
         };
-<<<<<<< HEAD
-        // // Parses roleData to array to use in button creation
-        // const buttonComponents = [];
-        // for (let i in roleData[interaction.customId].button) {
-            // buttonComponents.push(roleData[interaction.customId].button[i]);
-        // };
-        
-        const selectMenuRow = new ActionRowBuilder()
-            .addComponents(
-                new SelectMenuBuilder()
-                    .setCustomId(interaction.customId + '-SelectMenu')
-                    .setPlaceholder(roleData[interaction.customId].menu.placeholder)
-                    .setMaxValues(selectMenuOptions.length)
-                    .setMinValues(0)
-                    .addOptions(
-                        selectMenuOptions
-                    ),
-            );
-        
-        const buttonRow = new ActionRowBuilder()
-        for (let i in buttonData) {
-            buttonRow.addComponents(
-                new ButtonBuilder()
-                    .setCustomId(buttonData[i].customId)
-                    .setLabel(buttonData[i].label)
-                    .setEmoji(buttonData[i].emoji)
-                    .setStyle(buttonData[i].style),
-            );
-        }
-
-=======
 
         // // Parses roleData to array to use in button creation
         // const buttonComponents = [];
@@ -90,22 +53,13 @@ async function makeButtons (interaction) {
             );
         }
 
->>>>>>> dev
         await interaction.update({
             content: roleData[interaction.customId].menu.message,
             components: [selectMenuRow, buttonRow]
         });
-<<<<<<< HEAD
-        interaction.reply({content: '<a:aRight:978722165832695849> Your roles have been updated!', ephemeral: true});
-    } catch {
-        console.error(error);
-    }
-}
-=======
     } catch {
         console.log('[ROLE MANAGER] Failed to create/display role manager')
     }
 };
->>>>>>> dev
 
 module.exports = {makeButtons}
